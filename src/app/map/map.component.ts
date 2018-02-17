73cde95a7b655ccfbc7aa03a0ae1417e877f85d6
can3lita <lizzy.mb@gmail.com>
2018-02-17 10:18:16 -0400

    removed services map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MapService } from './map.service';
 import { MouseEvent } from '@agm/core';
 
 import { Incident } from './../incident/incident';
