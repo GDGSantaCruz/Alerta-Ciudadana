5a1ed6363cf51b0f920add84989787cc08008870
can3lita <lizzy.mb@gmail.com>
2018-02-24 02:26:13 -0400

    read from firestore

--- src/app/incident/incident.service.ts
+++ src/app/incident/incident.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Incident } from './incident';
-import { INCIDENTS } from './incident.mock';
+//import { INCIDENTS } from './incident.mock';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class IncidentService {
-    constructor() {}
+    constructor(private db: AngularFirestore) {}
 
-    getIncidents(): Incident[] {
-        return INCIDENTS;
+    getIncidents(): Observable<any[]> {
+        let since = new Date;
+        since.setDate(since.getDate() - 3);
+        console.log(since);
+        var incidentsRef = this.db.collection('Incidents', ref => ref.where("dateTime", ">=", since));
+        // incidentsRef.where("dateTime", ">=", since );
+        return incidentsRef.valueChanges();
     }
+
+
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MouseEvent } from '@agm/core';
+import { Observable } from 'rxjs/Observable';
 
 import { Incident } from './../incident/incident';
 import { IncidentService } from './../incident/incident.service';
@@ -14,11 +15,12 @@ export class MapComponent implements OnInit {
   longitude: number = -63.180234;
   zoom: number = 8;
   minZoom: number = 8;
-  incidents: Incident[] = [];
+  incidents: Observable<any[]>;
   constructor(private incidentService: IncidentService) {}
 
   ngOnInit() {
     this.incidents = this.incidentService.getIncidents();
+    console.log(this.incidents);
   }
 
   clickedMarker(incident: Incident) {
